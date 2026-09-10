cbabf6661b47ef92ff5ad9d63ed05018d1b9fe35
Amaranthusss <oskar.szkurlat@gmail.com>
2026-09-10 19:14:08 +0200

    Optimized Homepage image rendering performance

--- src/components/homepage-image/homepage-image.tsx
+++ src/components/homepage-image/homepage-image.tsx
@@ -10,12 +10,13 @@ import styles from './homepage-image.module.scss';
 export function HomepageImage({ theme }: HomepageImageProps): React.ReactNode {
   const src =
     theme === Theme.Dark
-      ? '/images/homepage-dark.png'
-      : '/images/homepage-light.png';
+      ? '/images/homepage-dark.webp'
+      : '/images/homepage-light.webp';
 
   const quality = 100;
   const width = 1219;
   const height = 756;
+  const sizes = '(max-width: 1280px) calc(100vw - 5rem), 1200px';
   const loading = 'eager' satisfies 'eager' | 'lazy';
 
   if (theme === Theme.System) {
@@ -23,17 +24,19 @@ export function HomepageImage({ theme }: HomepageImageProps): React.ReactNode {
       <picture>
         <source
           media={'(prefers-color-scheme: dark)'}
-          srcSet={'/images/homepage-dark.png'}
+          srcSet={'/images/homepage-dark.webp'}
         />
         <Image
           priority
-          src={'/images/homepage-light.png'}
+          src={'/images/homepage-light.webp'}
           alt={'Homepage background image'}
           className={styles.welcome_image}
+          fetchPriority={'high'}
           loading={loading}
           quality={quality}
           width={width}
           height={height}
+          sizes={sizes}
         />
       </picture>
     );
@@ -45,10 +48,12 @@ export function HomepageImage({ theme }: HomepageImageProps): React.ReactNode {
       src={src}
       alt={'Homepage background image'}
       className={styles.welcome_image}
+      fetchPriority={'high'}
       loading={loading}
       quality={quality}
       width={width}
       height={height}
+      sizes={sizes}
     />
   );
 }
